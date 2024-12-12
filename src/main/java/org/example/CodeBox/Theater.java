247c7b0a8ba22429abb23da5fe3cb5113cefefd2
pear_c <zxcv9676@naver.com>
2024-12-12 21:18:38 +0900

    [codeIt Pre-course] CodeBox

--- src/main/java/org/example/CodeBox/Theater.java
+++ src/main/java/org/example/CodeBox/Theater.java
@@ -15,6 +15,64 @@ public class Theater {
             }
         }
     }
+    // (5) 입력 문자 -> int 변환
+    private int getRowIndex(char uppercaseChar) {
+        return uppercaseChar - 'A';
+    }
+    // (1) 예약 메소드
+    public boolean reserve(String name, char rowChar, int col, int numSeat) {
+        if(rowChar - 'A' > rowCount || rowChar < 'A' || col > colCount || (col+numSeat-1) > colCount || col < 0)
+            return false;
+
+        for(int i=col-1; i<col+numSeat-1; i++) {
+            if(seats[getRowIndex(rowChar)][i].isOccupied())
+                return false;
+        }
+
+        for(int i=col-1; i<col+numSeat-1; i++) {
+            seats[getRowIndex(rowChar)][i].reserve(name);
+        }
+        return true;
+    }
+    // (2) 취소 메소드 - 취소된 좌석 수 반환
+    public int cancel(String name) {
+        int count = 0;
+        for(int i=0; i<rowCount; i++) {
+            for(int j=0; j<colCount; j++) {
+                if(seats[i][j].isOccupied()) {
+                    if(seats[i][j].match(name)) {
+                        seats[i][j].cancel();
+                        count++;
+                    }
+                }
+            }
+        }
+        return count;
+    }
+    // (3) 취소 메소드 - 열, 행, 좌석수에 맞는 모든 좌석 예약 취소 후 개수 반환
+    public int cancel(char rowChar, int col, int numSeat) {
+        int count = 0;
+        for(int i=col-1; i<col+numSeat-1; i++) {
+            if(i >= colCount) break;
+
+            if(seats[getRowIndex(rowChar)][i].isOccupied()) {
+                seats[getRowIndex(rowChar)][i].cancel();
+                count++;
+            }
+        }
+        return count;
+    }
+    // (4) 예약된 모든 좌석 수 리턴
+    public int getNumberOfReservedSeat() {
+        int count = 0;
+        for(int i=0; i<rowCount; i++) {
+            for(int j=0; j<colCount; j++) {
+                if(seats[i][j].isOccupied()) count++;
+            }
+        }
+        return count;
+    }
+
 
 
     public void printSeatMatrix() {
