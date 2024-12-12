7ff40d8093cd9381a1dc7ff70e9eedde81c8b5fb
pear_c <zxcv9676@naver.com>
2024-12-12 14:10:14 +0900

    [codeIt Pre-course] 삼각형 그리기

---
+++ src/main/java/org/example/ShapePrinter.java
@@ -0,0 +1,12 @@
+package org.example;
+
+public class ShapePrinter {
+    public void printTriangle(int height) {
+        for(int i=0; i<height; i++) {
+            for(int j=0; j<=i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}
