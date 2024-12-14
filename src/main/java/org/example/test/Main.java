c2562cdd09c48668c4f92a95cba975b4abeb973c
pear_c <zxcv9676@naver.com>
2024-12-14 22:18:55 +0900

    [codeIt Pre-course] test

---
+++ src/main/java/org/example/test/Main.java
@@ -0,0 +1,8 @@
+package org.example.test;
+
+public class Main {
+    public static void main(String[] args) {
+        testClass tc = new testClass("배성환", 25);
+
+    }
+}
---
+++ src/main/java/org/example/test/testClass.java
@@ -0,0 +1,11 @@
+package org.example.test;
+
+public class testClass {
+    private String name;
+    private int age;
+
+    public testClass(String name, int age) {
+        this.name = name;
+        this.age = age;
+    }
+}
